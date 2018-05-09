2718879edf9eae0b9f695b231ef613a70ee5c243
Avery Katko <averykatko@gmail.com>
2018-05-08 20:02:51 -0400

    add doc comments to dictype

--- prolog/dictype.pl
+++ prolog/dictype.pl
@@ -1,3 +1,9 @@
+/** <module>
+dictype - a directive for concise dict type definition
+@author Avery Katko
+@version 0.0.1
+@license MIT
+*/
 :- module(dictype, [dictype/1, op(1150, fx, dictype)]).
 
 :- use_module(library(error)).
@@ -6,9 +12,11 @@
 
 :- op(1150, fx, dictype).
 
+%! dict_type(Dict:dict, Type:dict).
+% body for has_type clauses asserted by dictype directive
 dict_type(Dict, Type) :-
-	is_dict(Dict),
-	is_dict(Type),
+	is_dict(Dict, Tag),
+	is_dict(Type, Tag),
 	dict_pairs(Dict, Tag, DictPairs),
 	dict_pairs(Type, Tag, TypePairs),
 	keysort(DictPairs, DictSorted),
@@ -17,6 +25,8 @@ dict_type(Dict, Type) :-
 	pairs_keys_values(TypeSorted, Keys, TypeValues),
 	maplist(the, TypeValues, DictValues).
 
+%! dictype(+Spec:compound) is det
+% directive to register dict types specified by dicts of the form typename{field1: type1, field2: type2, ...}
 dictype Spec :-
 	is_dict(Spec, Tag),
 	assertz((error:has_type(Tag, Dict) :- dict_type(Dict, Spec))).
